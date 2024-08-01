c2222f65b76474ca8ad108d9324ad8c3576fcfcc
IftekharAlamMithu <iftekharalammithu23@gmail.com>
2024-08-01 21:42:20 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
       <h1 className="text-red-400">sdfsd</h1>
       <p className="bg-sky-500">mmm</p>
       <Home></Home>
-      <p className="bg-yellow-500">mmm</p>
     </>
   );
 }
